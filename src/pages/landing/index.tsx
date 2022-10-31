fed515578ee74fc34db4a8a550cd26c2d56665c6
Eugenia Alcaraz <eugenia.alcaraz@rootstrap.com>
2022-10-31 17:43:10 -0300

    Adjust default route to redirect to sign-in

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Icon, Navbar } from "@components/common";
 import Phone from "@assets/images/i6.png";
 import Play from "@assets/images/play.png";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import { openMenu, closeMenu } from "@/state/features/menu/menuSlice";
 import { useAppDispatch, useAppSelector } from "@/state/app/hooks";
@@ -15,9 +15,18 @@ const Landing = () => {
   const dispatch = useAppDispatch();
   const { menuOpen, activeLink } = useAppSelector((state) => state.menu);
 
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const handleMenu = (status: boolean) => {
     status ? dispatch(openMenu()) : dispatch(closeMenu());
   };
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/sign-in");
+    }
+  }, []);
 
   return (
     <div className={styles.mainContainer}>
