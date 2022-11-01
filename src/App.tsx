7e0b5558520f1a8f35058d57dfe75a12418b1667
Eugenia Alcaraz <eugenia.alcaraz@rootstrap.com>
2022-11-01 18:19:36 -0300

    Adjust absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { About, SignIn, SignUp } from "@components/index";
-import { Landing, Pages } from "./pages";
-import { urlFormat } from "./utils/urlFormat";
+import { Landing, Pages } from "@/pages";
+import { urlFormat } from "@/utils/urlFormat";
 
 const App = () => (
   /*Private routing will be added once main screen is done*/
