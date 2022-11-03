b8bcafbed5cd57e30d65ae7a408e598fed2fb9fd
Eugenia Alcaraz <eugenia.alcaraz@rootstrap.com>
2022-11-03 17:06:27 -0300

    Add logout functionality

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { clearLocalStorage, urlFormat } from "@/utils";
+import { Pages } from "@/pages";
 
-const Main = () => {
-  return <div>Main</div>;
-};
+const Main = () => (
+  <Link to={urlFormat(Pages.SignIn)} onClick={() => clearLocalStorage()}>
+    Logout
+  </Link>
+);
 
 export { Main };
