dfa4ed7c58fb0375a102c14f1d68e59263e33bef
Eugenia Alcaraz <eugenia.alcaraz@rootstrap.com>
2022-11-09 10:40:44 -0300

    Adjust styling with classes

--- src/components/main/welcome/index.tsx
+++ src/components/main/welcome/index.tsx
@@ -8,20 +8,21 @@ const Welcome = () => {
       <div className={`${styles.headlines} flex-column`}>
         <Icon name="logo" />
         <h1>
-          Welcome to <strong>TARGET</strong>
+          Welcome to <strong className={styles.brandName}>TARGET</strong>
         </h1>
         <h2>Find people near you & Connect</h2>
       </div>
-      <ul>
-        <li>
+      <ul className={styles.content}>
+        <li className={styles.contentItem}>
           <p>
             Create a target by clicking wherever on the map, specify the ratio
             and and a topic: Travel, Dating, Music, etc.
           </p>
         </li>
-        <li>
+        <li className={styles.contentItem}>
           <p>
-            <strong>TARGET </strong>will start a chat whenever you’ve a match.
+            <strong className={styles.brandName}>TARGET </strong>will start a
+            chat whenever you’ve a match.
             <br />
             You can always dismiss a conversation if you’re not interested.
           </p>
